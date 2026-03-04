bfad1058aa3b274dff0b5f02cdf9c4fb9eb554e5
Dave Wentzel <davew@microsoft.com>
2026-03-03 20:12:53 -0500

    Committing 1 item from workspace 99328097-99f9-47f7-bb76-63191a1432c5

---
+++ VibeAnalytics/Notebook_14.Notebook/notebook-content.py
@@ -0,0 +1,31 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "7BD5BF37-B719-4BC8-81A8-1F88FD0B2046",
+# META       "default_lakehouse_name": "VibeAnalytics",
+# META       "default_lakehouse_workspace_id": "99328097-99F9-47F7-BB76-63191A1432C5",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "7BD5BF37-B719-4BC8-81A8-1F88FD0B2046"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+1+2
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
